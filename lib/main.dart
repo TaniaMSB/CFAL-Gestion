1431ff5d05b0119baea22dc7b8c8d661ebadb194
TaniaMSB <castromantilla3107@gmail.com>
2025-06-27 21:51:30 -0500

    se añadio firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:cfal_gestion/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
